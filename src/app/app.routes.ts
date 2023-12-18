e5f09de5dd8a6f9520e402d610e0de78b0b5c54e
Gustavo Lucianelli <gustavolucianelli993@gmail.com>
2023-12-18 15:41:58 -0300

    finished routes class app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,7 @@ import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found/page-not-found.component';
 import { AuthGuard } from "./auth-guard.service";
 import { CanDeactivateGuard } from "./servers/edit-server/can-deactive-guard.service";
+import { ErrorPageComponent } from "./error-page/error-page.component";
 
 export const appRoutes: Routes =[
   {path: '', component: HomeComponent},
@@ -22,6 +23,7 @@ export const appRoutes: Routes =[
       {path: ':id', component: ServerComponent},
       {path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard]}
     ]},
-    {path: 'not-found', component: PageNotFoundComponent},
+    // {path: 'not-found', component: PageNotFoundComponent},
+    {path: 'not-found', component: ErrorPageComponent, data:{message: 'TEST! Page not found!'}},
     {path: '**', redirectTo: '/not-found'}
 ]
